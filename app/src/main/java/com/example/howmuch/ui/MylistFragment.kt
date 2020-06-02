ea936efc172f4472fbbfbcb9264490a3438bf642
ta2lor <ksh262011@naver.com>
2020-06-02 23:47:22 +0900

    6/2 commit

--- app/src/main/java/com/example/howmuch/ui/MylistFragment.kt
+++ app/src/main/java/com/example/howmuch/ui/MylistFragment.kt
@@ -6,7 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.howmuch.ItemAdapter
+import com.example.howmuch.MainActivity
 import com.example.howmuch.R
+import kotlinx.android.synthetic.main.fragment_itemlist.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -20,6 +24,17 @@ class MylistFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_mylist, container, false)
     }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
+        val items = (requireContext() as MainActivity).itemRepository
+
+        view?.let{
+            it.message_rv.adapter = ItemAdapter(items)
+            it.message_rv.layoutManager = LinearLayoutManager(requireContext())
+
+
+        }
+    }
 
 }
